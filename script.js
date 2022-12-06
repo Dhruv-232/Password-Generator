90641d46ca341bea020fa24dad3a4c416e470199
Dhruv-232 <dhruvsharma2004@outlook.com>
2022-12-06 23:08:24 +1100

    Finished writing generatePassword() function

--- script.js
+++ script.js
@@ -1,35 +1,55 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var specialCharacters = ["!"]
-var upperCase = ["A"]
-var lowerCase = ["a"]
-var numeric = ["1"]
+var specialCharacters = ["!", "~", "!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "=", "`", "|"]
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var passwordLength = 0
 function getPasswordLength() {
   passwordLength = prompt("Please enter password length between 8 and 128 characters", "8-128")
-  if (7 < passwordLength && passwordLength < 129) {
-    alert("Good job")
+  if (8 > passwordLength || passwordLength > 128) {
+    alert("The password length entered should be 8-128 characters in length. Please try again")
+    getPasswordLength()
   }
 
-  else{
-    alert("bad")
-  }
-  alert("The chosen word length is "+passwordLength)
 }
 
 function generatePassword() {
-    upperCase = confirm("Do you want uppercase letters?")
-    lowerCase = confirm("Do you want lowercase letters?")
-    numeric = confirm("Do you want numeric letters?")
-    specialCharacters = confirm("Do you want special characters?")
-    
-    if (upperCase || lowerCase || numeric || specialCharacters === true) {
+    var includeUpperCase = confirm("Do you want uppercase letters?")
+    var includeLowerCase = confirm("Do you want lowercase letters?")
+    var includeNumeric = confirm("Do you want numeric letters?")
+    var includeSpecialCharacters = confirm("Do you want special characters?")
+    if (includeUpperCase || includeLowerCase || includeNumeric || includeSpecialCharacters === true) {
       getPasswordLength()
     }
     else {
       alert("Please select one of the prompts.")
       generatePassword()
     }
+
+    var charsToInclude = [];
+    console.log("Valid password criteria obtained ready to generate random password")
+    if (includeUpperCase === true) {
+      charsToInclude = charsToInclude.concat(upperCase);
+    }
+    if (includeLowerCase === true) {
+      charsToInclude = charsToInclude.concat(lowerCase);
+    }
+    if (includeNumeric === true) {
+      charsToInclude = charsToInclude.concat(numeric);
+    }
+    if (includeSpecialCharacters === true) {
+      charsToInclude = charsToInclude.concat(specialCharacters);
+    }
+    var generatedPassword = "";
+    var randomIndex;
+
+    for (var i = 0; i < passwordLength; i++) {
+      randomIndex = Math.floor(Math.random()*charsToInclude.length);
+      var randomChar = charsToInclude[randomIndex];
+      generatedPassword += randomChar;
+    }
+    return generatedPassword
 }
 // Write password to the #password input
 function writePassword() {

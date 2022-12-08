f61665161c0a5c72f4790c274ac1ee639d785aa4
Dhruv-232 <dhruvsharma2004@outlook.com>
2022-12-08 21:59:51 +1100

    Finished final editing of code

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 // Generating arrays of characters to be used in the generation of the password
-var specialCharacters = [".", ",", "'", " ", "(", ")", "{", "}", "~", "!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "=", "`", "|"]
+var specialCharacters = [".", ",", "'", " ", "(", ")", "{", "}", "~", "!", "@", "\"", "#", "$", "%", "^", "&", "*", "_", "-", "+", "=", "`", "|"]
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
